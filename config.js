af3ed6e2ea05d6f229ed51b2b2d73c1e949a0dab
Rishiidc <62295201+Rishiidc@users.noreply.github.com>
2021-01-16 17:02:06 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+require('@firebase/firestore')
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAOCDQMwwU0uqkJduRUpKD06hRcaidNJsY",
+  authDomain: "booksanta-fcee7.firebaseapp.com",
+  databaseURL: "https://booksanta-fcee7.firebaseio.com",
+  projectId: "booksanta-fcee7",
+  storageBucket: "booksanta-fcee7.appspot.com",
+  messagingSenderId: "996295504976",
+  appId: "1:996295504976:web:dac9907ee7d660f7ef4d55"
+};
+// Initialize Firebase
+  
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase.firestore();
